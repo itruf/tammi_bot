6fb0aa2915cc812f4d97d8e331e9b3be0e888507
Ivan Trufanov <ivan.trufanov@me.com>
2015-08-04 00:19:04 +0300

    d

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var express = require('express')
 var app = express()
 
+var url = "https://api.telegram.org/bot122550858:AAGfRzH0ikp6YqLCXr4hQ-yWEUtfu1NfN-M/"
+
+//Basic URL's
+/*
+	https://api.telegram.org/bot122550858:AAGfRzH0ikp6YqLCXr4hQ-yWEUtfu1NfN-M/sendMessage?chat_id=159371&text=eee
+	
+*/
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/hook', function(request, response) {
+	
+   var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          response.send('Response: ' + chunk);
+      });
+  });
 })
 
-app.get('/takeoff',function(request, response) {
-	response.send('Take off');	
+app.get('/', function(request, response) {
+  response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
