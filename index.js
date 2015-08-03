05f7d8efed5ab103b270472eb1f4a611b1a3adad
Ivan Trufanov <ivan.trufanov@me.com>
2015-08-04 00:24:21 +0300

    https

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
-var http = require('http');
+var https = require('https');
 
-var url = "https://api.telegram.org/bot122550858:AAGfRzH0ikp6YqLCXr4hQ-yWEUtfu1NfN-M/"
+var hostTelgram = "api.telegram.org"
+var telegramPathBasic = "/bot122550858:AAGfRzH0ikp6YqLCXr4hQ-yWEUtfu1NfN-M/"
 
 //Basic URL's
 /*
@@ -14,7 +15,26 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/hook', function(request, response) {
-	
+	https.get({
+        host: 'api.telegram.org',
+        path: telegramPathBasic + 'sendMessage?chat_id=159371&text=eee'
+    }, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+
+            // Data reception is done, do whatever with it!
+            var parsed = JSON.parse(body);
+            callback({
+                email: parsed.email,
+                password: parsed.pass
+            });
+        });
+    });
+    
    var post_req = http.request(post_options, function(res) {
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
